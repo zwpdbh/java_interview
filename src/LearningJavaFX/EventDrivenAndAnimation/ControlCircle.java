2cf7bb7f6b532d273cfaff0efc89c8f2c8bae732
zwpdbh <hyperion_z@outlook.com>
2018-08-04 22:59:35 +0800

    > LISTING 15.3 ControlCircle.java

---
+++ src/LearningJavaFX/EventDrivenAndAnimation/ControlCircle.java
@@ -0,0 +1,12 @@
+package LearningJavaFX.EventDrivenAndAnimation;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ControlCircle extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
