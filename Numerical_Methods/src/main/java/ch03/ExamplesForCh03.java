382978fdf34bf029a6d7b2e06638f379ad0bf52a
zwpdbh <hyperion_z@outlook.com>
2018-08-22 23:01:01 +0800

    > 3.4 Round off error

--- Numerical_Methods/src/main/java/ch03/ExamplesForCh03.java
+++ Numerical_Methods/src/main/java/ch03/ExamplesForCh03.java
@@ -1,12 +1,49 @@
 package ch03;
 
+import common.Common;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExamplesForCh03 {
 
     public static void main(String[] args) {
 
+        exampleErrorEstimatesForLterativeMethods();
+
+        System.out.println();
+        double estimatedResult = Common.iterComputeApproximatedE(1, 0.000001, 100);
+        System.out.println("estimated_E = " + estimatedResult);
     }
 
-    private static void errorEstimatesForLterativeMethods() {
+    /**Example from example 3.2: Error Estimates for iterative methods
+     * compute e^0.5 conforming to three significant figures*/
+    private static void exampleErrorEstimatesForLterativeMethods() {
+        // from 3 significant figures
+        double errorS = Common.getApproximationErrorFromNumberOfSignificantFigures(3);
+        System.out.println(errorS);
 
+        List<Double> approximationResult = Common.approximatedE(0.5, 10);
+        System.out.println(approximationResult);
+
+        System.out.println("Terms\tResult\terror_truth\terror_approximation");
+        List<Double> relativeTruthErrors = new ArrayList<Double>();
+        for (int i = 1; i <= approximationResult.size(); i++) {
+            relativeTruthErrors.add(Common.relativeTruthError(Math.exp(0.5), approximationResult.get(i-1)));
+        }
+        List<Double> relativeApproximationErrors = new ArrayList<Double>();
+        for (int i = 0; i < relativeTruthErrors.size(); i++) {
+            if (i == 0) {
+                relativeApproximationErrors.add(-1.0);
+            } else {
+                relativeApproximationErrors.add(
+                        Common.relativeApproximationError(approximationResult.get(i), approximationResult.get(i-1)));
+            }
+        }
+        for (int i = 0; i < relativeApproximationErrors.size(); i++) {
+            System.out.println((i+1) + "\t" + approximationResult.get(i) + "\t"
+                    + relativeTruthErrors.get(i) * 100 + "%" + "\t"
+                    + relativeApproximationErrors.get(i) * 100 + "%");
+        }
     }
 }
--- Numerical_Methods/src/main/java/common/Common.java
+++ Numerical_Methods/src/main/java/common/Common.java
@@ -1,10 +1,72 @@
 package common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Common {
     /**Helper function relate the relative specified approximation error to
      * the number of significant figures in the approximation
      * @param n which specify at least n significant figures*/
-    public static double specifiedApproximationErrorFrom(int n) {
-        return (0.5 * Math.pow(10, 2 - n));
+    public static double getApproximationErrorFromNumberOfSignificantFigures(int n) {
+        return (0.5 * Math.pow(10, 2 - n)) * 0.01;
+    }
+
+    /**Given a x and number of items to approximate, returns the list of results of approximation:
+     * e^x = 1
+     * e^x = 1 + x
+     * e^x = 1 + x + x^2/2
+     * e^x = 1 + x + x^2/2 + x^3/3!
+     * ...
+     * e^x = 1 + x + ... + x^n/n!
+     * It stores all the result into an array*/
+    public static List<Double> approximatedE(double x, int numberOfItems) {
+        List<Double> result = new ArrayList<Double>();
+        for (int i = 0; i < numberOfItems; i++) {
+            if (i == 0) {
+                result.add(1.0);
+            } else {
+                result.add((Math.pow(x, i) / factorial(i)) + result.get(result.size() - 1));
+            }
+        }
+        return result;
+    }
+
+    /**Use loop to compute the approximated E^(x) with:
+     * @param x is the parameter E^(x)
+     * @param errorCriteria is the target relative approximateError measured with by %.
+     * @param maxLoopNumber is the max loop will run in case the target approximationError is too small*/
+    public static double iterComputeApproximatedE(double x, double errorCriteria, double maxLoopNumber) {
+        double result = 1;
+        double prevousResult;
+        double ea;
+        for (int k = 1; k <= maxLoopNumber; k++) {
+            prevousResult = result;
+            result = result + Math.pow(x, k) / factorial(k);
+            ea = (Math.abs(result - prevousResult) / result ) * 100;
+            if (ea <= errorCriteria) {
+                System.out.println("k = " + k);
+                double errorTruth = Math.abs(Math.exp(1) - result) / Math.exp(1) * 100;
+                System.out.println("ea = " + ea);
+                System.out.println("et = " + errorTruth);
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static double factorial(int n) {
+        double result = 1.0;
+        for (int i = 1; i <= n; i++ ) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    public static Double relativeTruthError(double etruth, double eApproximation) {
+        return (Math.abs(eApproximation - etruth) / etruth);
+    }
+
+    public static Double relativeApproximationError(double currentApproximation, double previousApproximation) {
+        return (Math.abs(currentApproximation - previousApproximation) / currentApproximation);
     }
 }
